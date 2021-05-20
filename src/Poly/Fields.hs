31bcc8de1e69abb4e6cd9c90e2f8c4724fea6241
neclitoris <nas140301@gmail.com>
2021-05-20 21:57:09 +0300

    Better `Show` for Q and GF Now printed polynomials can be easily copied and pasted into code.

--- src/Poly/Fields.hs
+++ src/Poly/Fields.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -24,25 +25,19 @@ import Language.Haskell.TH qualified as TH
 import Poly.Polynomial
 
 
-type Q = Rational
+newtype Q = Q Rational deriving (Eq, Ord, Num, Fractional, Real, RealFrac)
 
 (%%) :: PolynomialConstraint (Polynomial Q v o) => Integer -> Integer -> Polynomial Q v o
-(%%) n = toPolynomial . (%) n
+(%%) n = toPolynomial . Q . (%) n
 
-type family Prime (n :: Nat) :: Constraint
+instance Show Q where
+  show (Q r) = show (numerator r) ++ "%%" ++ show (denominator r)
 
--- Primality test of type level naturals is beyond me.
--- Use this to ensure primality.
-assertPrimality :: Integer -> TH.Q [TH.Dec]
-assertPrimality n =
-  if not (isPrime n)
-     then error $ show n ++ " is not prime"
-     else [d| type instance Prime $(TH.litT $ TH.numTyLit n) = () |]
-    where
-      isPrime n = and [ n `mod` d /= 0 | d <- takeWhile (\x -> x * x <= n) [2..]]
 
-newtype GF (p :: Nat) = GF Natural deriving (Show, Eq)
+newtype GF (p :: Nat) = GF Natural deriving Eq
 
+instance Show (GF p) where
+  show (GF n) = show n
 
 instance (1 <= p, SingI p) => Num (GF p) where
   GF n + GF m = GF ((n + m) `mod` FromSing (sing :: Sing p))
@@ -56,6 +51,8 @@ instance (1 <= p, SingI p) => Num (GF p) where
   abs    = id
   signum = const $ GF 1
 
+type family Prime (n :: Nat) :: Constraint
+
 instance (Num (GF p), SingI p, Prime p) => Fractional (GF p) where
   recip (GF n) = let GCDEx x _ _ = gcdex (naturalToInteger n) p
                      p = naturalToInteger $ FromSing (sing :: Sing p)
@@ -65,6 +62,16 @@ instance (Num (GF p), SingI p, Prime p) => Fractional (GF p) where
                        y = denominator r
                     in fromInteger x / fromInteger y
 
+
+-- Primality test of type level naturals is beyond me.
+-- Use this to ensure primality.
+assertPrimality :: Integer -> TH.Q [TH.Dec]
+assertPrimality n =
+  if not (isPrime n)
+     then error $ show n ++ " is not prime"
+     else [d| type instance Prime $(TH.litT $ TH.numTyLit n) = () |]
+    where
+      isPrime n = and [ n `mod` d /= 0 | d <- takeWhile (\x -> x * x <= n) [2..]]
 
 data GCDEx =
   GCDEx { gcdLhsCoef :: Integer
